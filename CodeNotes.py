21f678f141910730f3c691bdcc057d0f55bf670a
levyjm <levyjohnm@gmail.com>
2017-04-08 17:37:24 -0400

    Selecting language is now a quick_panel isntead of a window_panel; insert indentation is good now

--- CodeNotes.py
+++ CodeNotes.py
@@ -8,35 +8,32 @@ activating the plugin.
 import json
 import os
 import sublime
-import sublime_plugins
+import sublime_plugin
 
 
 
 class AddCommand(sublime_plugin.TextCommand):
-    """ This is the a child class of sublime_plugin to 
+    """ This is the a child class of sublime_plugin to
     access the TextCommand's capabilities.
     """
 
 
-    def run(self, edit, snippet):
+    def run(self, snippet):
         """This is the function invoked when view.run_command('add') is called"""
 
-        self.view.insert(edit, self.view.sel()[0].begin(), snippet)
-
-
+        self.view.run_command("insert_snippet", {"contents": snippet})
 
 class CodenotesCommand(sublime_plugin.WindowCommand):
     """ This is the default class invoked when
     CodeNotes is selected from the context menu
     """
 
+
     def paste_snippet(self, event, snippets):
         """Invokes the AddCommand class"""
 
-
         view = self.window.active_view()
         view.run_command('add', {"snippet": snippets[event]})
-        
 
 
     def search_snippet(self, lang):
@@ -56,13 +53,13 @@ class CodenotesCommand(sublime_plugin.WindowCommand):
 
 
         snippet_names = list(set().union(*(dict.keys() for
-                                      dict in snippets_object["snippets"][lang])))
+                                           dict in snippets_object["snippets"][lang])))
         snippet_values = list(set().union(*(dict.values() for
-                                      dict in snippets_object["snippets"][lang])))
+                                            dict in snippets_object["snippets"][lang])))
 
 
         self.window.show_quick_panel(snippet_names, lambda event: self.paste_snippet(
-                                             event, snippet_values), 0, 0)
+            event, snippet_values), 0, 0)
 
     @classmethod
     def add_snippet(cls, event, lang, name, lang_in_set):
@@ -99,7 +96,8 @@ class CodenotesCommand(sublime_plugin.WindowCommand):
 
 
     def get_snippet_name(self, event, lang, lang_in_set):
-        """Parses the name given by the user for the snippet's identifier"""
+        """Parses the name for the snippet to add"""
+
         name = event
 
         self.window.show_input_panel("Enter snippet here:",
@@ -111,57 +109,67 @@ class CodenotesCommand(sublime_plugin.WindowCommand):
                                          lang_in_set), 0, 0)
 
 
-    def check_language(self, event, option):
-        """Receives the language and determines if already in set"""
-
-        process_option = option
-
-        lang_in_set = True
+    def add_language(self, event, lang_file, lang_in_set):
+        """Adds the language file to the languages.txt file"""
 
         snippet_language = event
 
-        # Attempt to open the languages.txt file
-        try:
-            file_path = os.path.dirname(__file__)
-            file_name = os.path.join(file_path, 'data_files/languages.txt')
-            lang_file = open(file_name, "a+")
-            lang_file.seek(0)
-            lang_set = set(line.strip() for line in lang_file)
-        except (OSError, IOError) as exception:
-            print("Could not open, read, or create ",
-                  "languages.txt due to error: {0}".format(exception))
-            lang_set = set()
-
-        # check in set if language requested is in set and write if not
-        if snippet_language not in lang_set:
-            lang_file.write(snippet_language)
-            lang_file.write("\n")
-            lang_in_set = False
-
+        lang_file.write("\n")
+        lang_file.write(snippet_language)
         lang_file.close()
 
-        if process_option == 0:
-            self.window.show_input_panel("Enter a name for your new snippet:",
-                                         "", lambda event: self.get_snippet_name(
-                                             event, snippet_language, lang_in_set), 0, 0)
-        elif process_option == 1:
-            self.search_snippet(snippet_language)
+        self.window.show_input_panel("Enter a name for your new snippet:",
+                                     "", lambda event: self.get_snippet_name(
+                                         event, snippet_language, lang_in_set), 0, 0)
+
+
+    def check_language(self, event, menu_option, lang_set, lang_file):
+        """Receives the language and determines if already in set"""
+
+        lang_in_set = True
+
+        if event == (len(lang_set) - 1):
+            lang_set.pop(len(lang_set) - 1)
+            lang_in_set = False
+            self.window.show_input_panel("Add language:",
+                                         "", lambda event: self.add_language(
+                                             event, lang_file, lang_in_set), 0, 0)
+        else:
+            snippet_language = lang_set[event]
+            if menu_option == 0:
+                self.window.show_input_panel("Enter a name for your new snippet:",
+                                             "", lambda event: self.get_snippet_name(
+                                                 event, snippet_language, lang_in_set), 0, 0)
+            elif menu_option == 1:
+                self.search_snippet(snippet_language)
+                lang_file.close()
 
 
     def process_option(self, event):
         """Determines what menu option was selected"""
 
+        try:
+            file_path = os.path.dirname(__file__)
+            file_name = os.path.join(file_path, 'data_files/languages.txt')
+            lang_file = open(file_name, "a+")
+            lang_file.seek(0)
+            lang_set = list(line.strip() for line in lang_file)
+        except (OSError, IOError) as exception:
+            print("Could not open, read, or create ",
+                  "languages.txt due to error: {0}".format(exception))
+            lang_set = set()
+
+        lang_set.append("Add new language")
+
         menu_option = 0
 
         if event == 0:
-            self.window.show_input_panel("Enter snippet language here:", 
-                                    "", lambda event: self.check_language(
-                                             event, menu_option), 0, 0)
+            self.window.show_quick_panel(lang_set, lambda event: self.check_language(
+                                             event, menu_option, lang_set, lang_file), 0, 0)
         elif event == 1:
             menu_option = 1
-            self.window.show_input_panel("Find your snippets for which language?", 
-                                         "", lambda event: self.check_language(
-                                             event, menu_option), 0, 0)
+            self.window.show_quick_panel(lang_set, lambda event: self.check_language(
+                                             event, menu_option, lang_set, lang_file), 0, 0)
 
 
     def run(self):
